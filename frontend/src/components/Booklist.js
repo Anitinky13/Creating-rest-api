b8b7938fd216d2820bd8cc4c38282ebd60cd8a44
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-11-03 13:27:27 -0500

    Finall product

--- frontend/src/components/Booklist.js
+++ frontend/src/components/Booklist.js
@@ -1,21 +1,18 @@
 import React, { useState, useEffect } from "react";
-import CalendarComponent from './calendar';
-import Book from './Book';
-import {
-  getBooks,
-  addNewBook
-} from '../api/books';
+import CalendarComponent from "./Calendar";
+import Book from "./Book";
+import { getBooks, addNewBook } from "../api/books";
 
 // Material UI
-import List from '@mui/material/List';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DatePicker from '@mui/lab/DatePicker'
-import Stack from '@mui/material/Stack';
+import List from "@mui/material/List";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DatePicker from "@mui/lab/DatePicker";
+import Stack from "@mui/material/Stack";
 
-import '../App.css';
+import "../App.css";
 
 const BookSchedule = () => {
   // GET from API
@@ -36,84 +33,81 @@ const BookSchedule = () => {
     }
     fetchData();
   }, []);
-      
+
   // Add a new book to the list
   const onAddNewBook = async () => {
     const newBook = await addNewBook(newBookTitle, newBookStart, newBookEnd);
 
-    setBooks((previousBooks) => [
-      ...previousBooks,
-      newBook,
-    ]);
+    setBooks((previousBooks) => [...previousBooks, newBook]);
   };
-
-
 
   // Map through the array of books and create a list item for them
   const bookTitle = books.map((book) => {
     return (
-      <Book  
-      bookId={book.id}
-      bookTitle={book.title}
-      bookStart={book.start}
-      bookEnd={book.end}
-      setBooks={setBooks}
+      <Book
+        bookId={book.id}
+        bookTitle={book.title}
+        bookStart={book.start}
+        bookEnd={book.end}
+        setBooks={setBooks}
       />
-    )
-  
+    );
   });
 
   return (
     <div>
       <CalendarComponent events={books} />
-        <h3>Add a New Book to Read!</h3>
-        <div className={'add-new'}>
-          <form onSubmit={() => {onAddNewBook()}}>
-              <Stack spacing={2}>
-                  <TextField id="standard-basic"
-                             required
-                             label="Add Book Title"
-                             variant="standard"
-                             onChange={e => setNewBookTitle(e.target.value)}
-                  />
-                  <LocalizationProvider dateAdapter={AdapterDateFns}>
-                      <DatePicker
-                          label="Start Date"
-                          openTo="day"
-                          views={['year', 'month', 'day']}
-                          value={startDate}
-                          onChange={(newValue) => {
-                              setStartDate(newValue);
-                              setNewBookStart(newValue);
-                          }}
-                          renderInput={(params) => <TextField {...params} />}
-                      />
-                      <DatePicker
-                          label="End Date"
-                          openTo="day"
-                          views={['year', 'month', 'day']}
-                          value={endDate}
-                          onChange={(newValue) => {
-                              setEndDate(newValue);
-                              setNewBookEnd(newValue);
-                          }}
-                          renderInput={(params) => <TextField {...params} />}
-                      />
-                  </LocalizationProvider>
-                  <Button variant="contained" type="submit">
-                      Add Book
-                  </Button>
-              </Stack>
-          </form>
-        </div>
-        <div className={'existing-books'}>
-          <h3>Existing Book Schedules</h3>
-          <List>
-            {bookTitle}
-          </List>
-        </div>
+      <h3>Add a New Book to Read!</h3>
+      <div className={"add-new"}>
+        <form
+          onSubmit={() => {
+            onAddNewBook();
+          }}
+        >
+          <Stack spacing={2}>
+            <TextField
+              id="standard-basic"
+              required
+              label="Add Book Title"
+              variant="standard"
+              onChange={(e) => setNewBookTitle(e.target.value)}
+            />
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Start Date"
+                openTo="day"
+                views={["year", "month", "day"]}
+                value={startDate}
+                onChange={(newValue) => {
+                  setStartDate(newValue);
+                  setNewBookStart(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+              <DatePicker
+                label="End Date"
+                openTo="day"
+                views={["year", "month", "day"]}
+                value={endDate}
+                onChange={(newValue) => {
+                  setEndDate(newValue);
+                  setNewBookEnd(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+            <Button variant="contained" type="submit">
+              Add Book
+            </Button>
+          </Stack>
+        </form>
+      </div>
+      <div className={"existing-books"}>
+        <h3>Existing Book Schedules</h3>
+        <List>{bookTitle}</List>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default BookSchedule;
